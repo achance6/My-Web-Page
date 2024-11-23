0fe946f7751a68df6465427e7db99ea12b70f6be
achance6 <ayden.chance@rocketmail.com>
2024-11-23 13:06:13 -0500

    Add Dark Mode button

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Happy developing ✨')
+document.getElementById("toggle-dark-mode-button")?.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark");
+    toggleDarkModeIndicator();
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    toggleDarkModeIndicator();
+})
+
+function toggleDarkModeIndicator() {
+    let darkModeToggleTextElement = document.getElementById("dark-mode-indicator");
+    if (!darkModeToggleTextElement) {
+        console.error("Dark mode button was undefined");
+        return;
+    }
+    if (document.documentElement.classList.contains("dark")) {
+        darkModeToggleTextElement.innerText = "ON";
+    } else {
+        darkModeToggleTextElement.innerText = "OFF";
+    }
+}
