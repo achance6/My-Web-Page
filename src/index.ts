090d462d8b78f008b93bb7dff2da9641c3375323
achance6 <ayden.chance@rocketmail.com>
2024-11-23 13:39:32 -0500

    Add local persistence of dark mode preference + minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
+const DARK_MODE_CLASS = "dark";
+const LIGHT_MODE_CLASS = "light";
+
 document.getElementById("toggle-dark-mode-button")?.addEventListener("click", () => {
-    document.documentElement.classList.toggle("dark");
-    toggleDarkModeIndicator();
+    const currentTheme = document.documentElement.className;
+    const newTheme = currentTheme === DARK_MODE_CLASS ? LIGHT_MODE_CLASS : DARK_MODE_CLASS;
+    document.documentElement.className = newTheme;
+    localStorage.setItem('theme', newTheme);
+    setDarkModeIndicator();
+
 })
 
 document.addEventListener("DOMContentLoaded", () => {
-    toggleDarkModeIndicator();
+    const userPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        document.documentElement.className = savedTheme;
+    } else {
+        document.documentElement.className = userPrefersDark ? DARK_MODE_CLASS : LIGHT_MODE_CLASS;
+    }
+    setDarkModeIndicator();
 })
 
-function toggleDarkModeIndicator() {
+function setDarkModeIndicator() {
     let darkModeToggleTextElement = document.getElementById("dark-mode-indicator");
     if (!darkModeToggleTextElement) {
-        console.error("Dark mode button was undefined");
+        console.error("Dark mode indicator was undefined");
         return;
     }
-    if (document.documentElement.classList.contains("dark")) {
-        darkModeToggleTextElement.innerText = "ON";
-    } else {
+    document.documentElement.className === DARK_MODE_CLASS ?
+        darkModeToggleTextElement.innerText = "ON" :
         darkModeToggleTextElement.innerText = "OFF";
-    }
 }
