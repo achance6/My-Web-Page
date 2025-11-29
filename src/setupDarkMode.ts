2ec9ace96a7abd61d4f0a6c07b5ed3c0bdaa8def
achance6 <ayden.chance@rocketmail.com>
2025-11-29 17:22:39 -0500

    Use BEM for CSS

--- src/setupDarkMode.ts
+++ src/setupDarkMode.ts
@@ -1,23 +1,16 @@
-export function setupDarkMode() {
-  const DARK_MODE_CLASS = "dark";
-  const LIGHT_MODE_CLASS = "light";
+const DARK_MODE_CLASS = "dark";
+const LIGHT_MODE_CLASS = "light";
+const THEME_STORAGE_KEY = "theme";
 
+export function setupDarkMode() {
   document
-    .getElementById("toggle-dark-mode-button")
-    ?.addEventListener("click", () => {
-      const currentTheme = document.documentElement.className;
-      const newTheme =
-        currentTheme === DARK_MODE_CLASS ? LIGHT_MODE_CLASS : DARK_MODE_CLASS;
-      document.documentElement.className = newTheme;
-      localStorage.setItem("theme", newTheme);
-      setDarkModeIndicator();
-    });
+    .querySelector(".dark-mode-toggle__button")
+    ?.addEventListener("click", darkModeToggle);
 
   document.addEventListener("DOMContentLoaded", () => {
-    const userPrefersDark = window.matchMedia(
-      "(prefers-color-scheme: dark)",
-    ).matches;
-    const savedTheme = localStorage.getItem("theme");
+    const userPrefersDark = matchMedia("(prefers-color-scheme: dark)").matches;
+
+    const savedTheme = localStorage.getItem(THEME_STORAGE_KEY);
     if (savedTheme) {
       document.documentElement.className = savedTheme;
     } else {
@@ -27,17 +20,32 @@ export function setupDarkMode() {
     }
     setDarkModeIndicator();
   });
+}
 
-  function setDarkModeIndicator() {
-    let darkModeToggleTextElement = document.getElementById(
-      "dark-mode-indicator",
-    );
-    if (!darkModeToggleTextElement) {
-      console.error("Dark mode indicator was undefined");
-      return;
-    }
-    document.documentElement.className === DARK_MODE_CLASS
-      ? (darkModeToggleTextElement.innerText = "ON")
-      : (darkModeToggleTextElement.innerText = "OFF");
+function darkModeToggle() {
+  const currentTheme = document.documentElement.className;
+  const newTheme =
+    currentTheme === DARK_MODE_CLASS ? LIGHT_MODE_CLASS : DARK_MODE_CLASS;
+  document.documentElement.className = newTheme;
+  localStorage.setItem(THEME_STORAGE_KEY, newTheme);
+  setDarkModeIndicator();
+}
+
+/**
+ * Sets the dark mode indicator text to either "ON" or "OFF"
+ */
+function setDarkModeIndicator() {
+  const darkModeToggleTextElement = document.querySelector(
+    ".dark-mode-toggle__indicator",
+  );
+  if (
+    !darkModeToggleTextElement ||
+    !(darkModeToggleTextElement instanceof HTMLElement)
+  ) {
+    console.error("Dark mode indicator was undefined");
+    return;
   }
+  document.documentElement.className === DARK_MODE_CLASS
+    ? (darkModeToggleTextElement.innerText = "ON")
+    : (darkModeToggleTextElement.innerText = "OFF");
 }
